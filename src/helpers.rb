4185fa018443596c702a4e775f8bd27267afe4e8
Aeshaan Wahlang <aeshaanwahlang@dhcp-10-4-1-255.wireless.rochester.edu>
2018-11-10 16:48:32 -0500

    more word on repo done

---
+++ src/helpers.rb
@@ -0,0 +1,26 @@
+require 'pathname'
+
+def os_walk(dir, ignore)
+  root = Pathname(dir)
+      files, dirs = [], []
+      Pathname(root).find do |path|
+        unless path == root
+            if !path.to_s.include? ignore
+              dirs << path if path.directory?
+              files << path if path.file?
+          end
+      end
+  end
+  [root, dirs, files]
+end
+
+def diff(path)
+  walk = os_walk(Dir.pwd, '.git')
+  for f in walk[2]
+    s = File.stat(f)
+    p s.mtime
+    break
+  end
+end
+
+diff(Dir.pwd)
--- src/repository.rb
+++ src/repository.rb
@@ -1,33 +1,99 @@
 require 'changelog'
+require 'helpers'
 
 class Repository
-    
-    # List of head revisions (chnageLogs??)
-    @heads = []
+    @path = ''
     # constructor
     # path-> path to Repo root
-    def initialize(path)
-        # create .jsaw folder with all relevant files 
-
-        # initilize head changeLog
-        @rootChangeLog = ChangeLog.new(self, path)
-        # initialize manifestLog
-
+    def initialize(path = nil, create = false)
+        # create .jsaw folder with all relevant files if required
+        path = Dir.pwd + "/" if !path
+        @path = path
+        @root = path
+        if create
+            Dir.mkdir(@path+".jsaw")
+            # TODO: make other files
+        end
+        # initilize head changeLog and minifest
+        @changeLog = ChangeLog.new(self, @path)
+        @manifest = Manifest.new(self, @path)
         # fileLogs???
     end
 
+    def open(path, mode = "r")
+        f = join(path)
+        if mode == "a" and File.file?(f)
+            s = File.stat(f)
+            if s.nlink > 1
+                # WTFF!!
+                # File.open(f+'.tmp', "w")
+            end
+        return File.open(join(path), mode)
+    end
+
+    def join(f)
+        return File.join(Dir.pwd, f)
+    end
+
+    def file(f)
+        return filelog(self, f)
+    end
     # commit method
     def commit(message)
-        # TODO
+        update = []
+        delete = []
+        begin
+            File.open('.jsaw/to-add').each_line{|l| update << l[0..-2]}
+            File.open('.jsaw/to-delete'.each_line{|l| delete << l[0..-2]}
+        end
+        rescue IOError
+            p "File error"
+        end
+        # check in files
+        new = {}
+        for f in update
+            r = filelog(self, f)
+            t = file(f).read()
+            r.addrevision(t)
+            new[f] = r.node(r.tip())
+        end
+        # update manifest
+        old = @manifest.manifest(@manifest.tip())
+        old.update(new)
+        for f in delete
+            old.delete(f)
+        end
+        rev = @manifest.addmanifest(old)
+        # add changeset
+        new = new.keys()
+        new.sort()
+        n = @changeset.addchangeset(@manifest.node(rev), new, "commit")
+        @current = n
+        # Some sketchy shit
+        # unlink path 
+    end
+
+    def dirdiff(path)
+        st = open("dircache").read()
+        dc = {}
+        pos = 0
 
     end
 
-    # method to return head Revision of Repo
-    def getHeadRevision()
-        # find and return head Revision
-
+    def add(list)
+        addlist = open('to-add', 'a')
+        state = open('dircache', 'a')
+        for f in list
+            addlist.write(f+'\n')
+            st = File.stat(f)
+            e = [st.mode, st.size, st.mtime, f.length, f]
+            e.each{|i| state.write(i)}
+        end
     end
 
-    # other methods .... 
+    def delete(list)
+        delList = open('to-delete', 'a')
+        list.each{|f| delList.write(f+'\n')}
+    end
 
 end
