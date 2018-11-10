4e5b59c799e76ccf21e897885635a4295ecf5a3f
jiao <jiao@dhcp-10-4-16-124.wireless.rochester.edu>
2018-11-10 16:24:03 -0500

    update manifest and delete exception

--- src/manifest.rb
+++ src/manifest.rb
@@ -1,45 +1,20 @@
 # filelog.rb created by Mingyang 11.4.18
 require 'digest'
 
-
-# class manifest(revlog):
-#     def __init__(self, repo):
-#         self.repo = repo
-#         revlog.__init__(self, "00manifest.i", "00manifest.d")
-#     def open(self, file, mode="r"):
-#         return self.repo.open(file, mode)
-#     def manifest(self, rev):
-#         text = self.revision(rev)
-#         map = {}
-#         for l in mdiff.linesplit(text):
-#             map[l[41:-1]] = binascii.unhexlify(l[0:40])
-#         return map
-#     def addmanifest(self, map, p1=None, p2=None):
-#         files = map.keys()
-#         files.sort()
-#         text = "".join(["%s %s\n" % (binascii.hexlify(map[f]), f)
-#                         for f in files])
-#         return self.addrevision(text, p1, p2)
-
 class Manifest < Revlog
     def initialize(repo,path)
-        # # self.repo = repo
+        self.repo = repo
         Revlog.new("00manifest.i", "00manifest.d")
     end
 
     def open(file, mode = "r")
-        # def open(self, file, mode = "r"):
-        # return self.repo.open(file, mode)
+        return self.repo.open(file, mode)
     end
 
     def manifest(rev)
-        text = self.revision(rev)
-        # map = {}
-        # for l in mdiff.linesplit(text):
-        #     map[l[41:-1]] = binascii.unhexlify(l[0:40])
-        # return map     
+        text = self.revision(rev)    
         hash = {}
-        text.split("\n").each do |e|
+        text.lines.each do |e|
             key = e.split(" ")[0]
             value = e.split(" ")[1]
             hash[key] = value
@@ -47,17 +22,11 @@ class Manifest < Revlog
         return hash
     end
 
-    def add_manifest(hash, p1 = nil, p2 = nil)
-        #         files = map.keys()
-        # files.sort()
-        # text = "".join(["%s %s\n" % (binascii.hexlify(map[f]), f)
-        #                 for f in files])
-        # return self.addrevision(text, p1, p2)
-        hash = hash.sort
-        hash.each {|key, value| arr << key + " " + value}
+    def add_manifest(file_hash, p1 = nil, p2 = nil)
+        file_hash = file_hash.sort
+        arr = []
+        file_hash.each {|name, name_hash| arr << name + " " + name_hash}
         text = arr.join("\n")
         return self.addrevision(text,p1,p2)
     end
 end
-
-# Filelog.new(repo = 6,"/Users/jiao/Desktop/DVCS")
