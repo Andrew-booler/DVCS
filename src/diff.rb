841124c45c82e26f8701caeec178f1c9d5d1444e
Simon Budker <sbudker@gmail.com>
2018-11-10 16:00:27 -0500

    Add DiffUtils module wiht filled in methods

--- src/diff.rb
+++ src/diff.rb
@@ -5,16 +5,26 @@ require 'diff/lcs'
 # p textdiff("aa\nbb\n", "bb\naa\n") # produces [[["-", 0, "aa\n"]], [["+", 1, "aa\n"]]]
 # p patch("aa\nbb\n", textdiff("aa\nbb\n", "bb\naa\n")) # produces ["bb\n", "aa\n"]
 #
-# include this in other files with `require_relative 'diff.rb'`
+# use this in other files like this:
+# require_relative 'diff.rb'
+# DiffUtils.linesplit("sss")
 
-def linesplit(str)
-    str.lines
-end
+module DiffUtils
 
-def textdiff(prev_str, new_str)
-    return Diff::LCS.diff(linesplit(prev_str), linesplit(new_str))
-end
+    def linesplit(str)
+        str.lines
+    end
 
-def patch(prev_str, diffs)
-    return Diff::LCS.patch!(linesplit(prev_str), diffs)
+    def textdiff(prev_str, new_str)
+        return Diff::LCS.diff(linesplit(prev_str), linesplit(new_str))
+    end
+
+    def patch(prev_str, diffs)
+        return Diff::LCS.patch!(linesplit(prev_str), diffs)
+    end
+
+    module_function :linesplit
+    module_function :textdiff
+    module_function :patch
+
 end
