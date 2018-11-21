9975606c69c174bd79200f628bf6cacbf08b016d
Simon Budker <sbudker@gmail.com>
2018-11-21 15:29:06 -0800

    Test for filelog

--- test/module/filelog_test.rb
+++ test/module/filelog_test.rb
@@ -1,10 +1,16 @@
 require "minitest/autorun"
 require_relative "../../src/filelog"
+require_relative "../../src/repository"
 
-class FileLogTest < Minitest::Test
-  def setup
-  end
+class FilelogTest < Minitest::Test
 
-  def test_init
-  end
+    def test_init
+        repo = Repository.new()
+        fl = Filelog.new(repo, "path")
+
+        # ensure file will go into data or index folder
+        assert_equal false, (fl.datafile =~ /data\/*{40}/).nil?
+        assert_equal false, (fl.indexfile =~ /index\/*{40}/).nil?
+    end
+
 end
