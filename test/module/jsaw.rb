723553bc80aa0f352548ade63bf2a1ad9d47bddf
Simon Budker <sbudker@gmail.com>
2018-11-21 15:58:00 -0800

    super simple test for jsaw script

--- test/module/jsaw.rb
+++ test/module/jsaw.rb
@@ -1,10 +1,18 @@
 require "minitest/autorun"
-require_relative "../../src/jsaw"
 
 class JSAWTest < Minitest::Test
-  def setup
-  end
 
-  def test_commit
-  end
+    def setup
+        Dir.chdir "src" if File.basename(Dir.getwd) != "src"
+    end
+
+    def test_invalid_command
+        out = `./jsaw dog`
+        assert_equal "jsaw: 'dog' is not a valid command. See 'jsaw help'.\n", out
+    end
+
+    def test_commit_requires_msg
+        out = `./jsaw commit`
+        assert_equal "You must have a message to commit\n", out
+    end
 end
